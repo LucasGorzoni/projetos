223fde1cf996073b92925f6d1b05a95773f7a1fc
LucasGorzoni <lucasgorzoni@gmail.com>
2020-12-09 13:17:17 -0300

    Primeiro commit

---
+++ testegit/src/test/java/com/lucas/crm/teste/TestegitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.crm.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestegitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
